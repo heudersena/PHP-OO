6603a6e2f3f79055927468102d031a782b1c6e9f
heudersena <documento_arquivo@hotmail.com>
2017-05-26 13:15:39 -0300

    método Polimorfismo variavel modificada

--- Aulla05/index.php
+++ Aulla05/index.php
@@ -6,5 +6,5 @@ include './Classes/FinanciamentoMoto.class.php';
 $carro = new FinanciamentoCarro(10000);
 $carro->CalcularJuros();
 echo "<hr>";
-$moto = new FinanciamentoMoto($valor);
+$moto = new FinanciamentoMoto(2000);
 $moto->CalcularJuros();

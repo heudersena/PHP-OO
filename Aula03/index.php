6ab486024d9bd23f37f0f775ed32291620523ef8
heudersena <documento_arquivo@hotmail.com>
2017-05-26 11:54:06 -0300

    Aula 03 Metodo __construct

---
+++ Aula03/index.php
@@ -0,0 +1,7 @@
+<?php 
+// Incluindo a class carro
+include './Classes/Carro.class.php';
+
+// Isntanciando a class carro
+$carro = new Carro('2017','Camaro','Amarelo','Chevrolet');
+$carro->MostraDados();
